99ecdec009e5a08ce619815382f5bd958774d1c5
Kaustubh Jogle <kaustubhrjogle@gmail.com>
2025-07-14 02:14:56 +0530

    Fix Hydration Error - only one html and body tag

--- app/(users)/about/layout.js
+++ app/(users)/about/layout.js
@@ -1,10 +1,8 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <h4>ABOUT SECTION</h4>
-        {children}
-      </body>
-    </html>
+    <>
+      <h4>ABOUT SECTION</h4>
+      {children}
+    </>
   );
 }
