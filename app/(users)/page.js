ccd5bc6e693f8953cabb97edcdb58fe92e4b6344
Kaustubh Jogle <kaustubhrjogle@gmail.com>
2025-07-14 01:25:48 +0530

    Use Image tag for rendering Images

--- app/(users)/page.js
+++ app/(users)/page.js
@@ -1,9 +1,33 @@
 import Image from "next/image";
+import robot_img from '../../public/robot_hi.jpg'
 
 export default function Home() {
   return (
     <div>
-      <h2>Initial Next JS Page</h2>
+      <div className="hero-section flex">
+        <div className="p-2 m-2 text-center">
+          <Image
+            src="/robot_hi.jpg"
+            width={400}
+            height={400}
+            alt="hero-section-image"
+          />
+          <p className="m-1">Default Quality</p>
+        </div>
+        <div className="p-2 m-2 text-center">
+          <Image
+            src={robot_img}
+            width={400}
+            height={400}
+            alt="hero-section-image"
+            quality={100}
+            priority={true}
+            blurDataURL=""
+            placeholder="blur"
+          />
+          <p className="m-1">High Quality</p>
+        </div>
+      </div>
     </div>
   );
 }
