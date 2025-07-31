d6b645c1da4c8062074ca72ad3ea107346e0cc9c
Kaustubh Jogle <kaustubhrjogle@gmail.com>
2025-08-01 01:22:10 +0530

    Implement React Cache to avoid unnecessary db calls

--- app/(users)/(mysql)/data/dynamicfetch/page.jsx
+++ app/(users)/(mysql)/data/dynamicfetch/page.jsx
@@ -1,27 +1,41 @@
 import React from "react";
+import { cache } from "react";
 import { db } from "@/config/mysql";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
+
+const getData = cache(async () => {
+  console.log("Getting data");
+  const [todoData] = await db.execute("SELECT * FROM todo_db.tasks");
+  return todoData;
+});
 
 const page = async () => {
-  const [todoData] = await db.execute("SELECT * FROM todo_db.tasks");
-
+  const todoData = await getData();
   return (
     <div className="flex justify-center text-center flex-col">
       <div>
         <p className="text-3xl m-5">Dynamic Data from MySql</p>
+        <p className="text-xl m-2">{todoData?.length} Items</p>
+        <ListComponent />
       </div>
-      <div>
-        <ul>
-          {todoData.map((item) => {
-            return (
-              <li key={item?.id}>
-                {item?.id} : {item?.title}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    </div>
+  );
+};
+
+const ListComponent = async () => {
+  const todoData = await getData();
+  return (
+    <div>
+      <ul>
+        {todoData.map((item) => {
+          return (
+            <li key={item?.id}>
+              {item?.id} : {item?.title}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
